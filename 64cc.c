f288f6588507d07d2f277de8e5599f9ed301b80b
Atsushi Koda <koda.atsushi.716@s.kyushu-u.ac.jp>
2022-09-10 15:48:16 +0000

    Add *, / and ()

--- 64cc.c
+++ 64cc.c
@@ -12,6 +12,16 @@ typedef enum
     TK_EOF       // 入力の終わりを表すトークン
 } TokenKind;
 
+// 抽象構文木のノードの種類
+typedef enum
+{
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 整数
+} NodeKind;
+
 typedef struct Token Token;
 
 struct Token
@@ -20,6 +30,17 @@ struct Token
     Token *next;    // 次の入力トークン
     int val;        // kindがTK_NUMの場合、その数値
     char *str;      // トークン文字列
+};
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node
+{
+    NodeKind kind; // ノードの型
+    Node *lhs; // 左辺 left-hand side
+    Node *rhs; // 右辺 right-hand side
+    int val; // kindがND_NUMの場合のみ使う
 };
 
 char *user_input;
@@ -121,7 +142,7 @@ Token *tokenize(char *p)
             continue;
         }
 
-        if (*p == '+' || *p == '-') {
+        if (strchr("+-*/()", *p)) {
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -139,6 +160,106 @@ Token *tokenize(char *p)
     return head.next;
 }
 
+// 新しいノードを作成する関数
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+// 新しいノードを作成する関数
+Node *new_node_number(int val)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+// 左結合の演算子をパースする関数
+// 生成規則: expr = mul ("+" mul | "-" mul)*
+Node *expr()
+{
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+'))
+            node = new_node(ND_ADD, node, mul()); 
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+
+// 生成規則: mul = primary ("*" primary | "/" primary)*
+Node *mul()
+{
+    Node *node = primary();
+
+    for (;;) {
+        if (consume('*'))
+            node = new_node(ND_MUL, node, primary()); 
+        else if (consume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+// 生成規則: primary = "(" expr ")" | num
+Node *primary()
+{
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    return new_node_number(expect_number());
+}
+
+void gen(Node *node)
+{
+    if (node->kind == ND_NUM) {
+        printf("  mov x0, #%d\n", node->val);
+        // アセンブリの"!"はregister write-backを意味する。以下の場合はsp=sp-16となる。
+        printf("  str x0, [sp, #-16]!\n");
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  ldr x1, [sp], #16\n");
+    printf("  ldr x0, [sp], #16\n");
+
+    switch (node->kind)
+    {
+    case ND_ADD:
+        printf("  add x0, x0, x1\n");
+        break;
+    case ND_SUB:
+        printf("  sub x0, x0, x1\n");
+        break;
+    case ND_MUL:
+        printf("  mul x0, x0, x1\n");
+        break;
+    case ND_DIV:
+        printf("  sdiv x0, x0, x1\n");
+        break;
+    }
+
+    printf("  str x0, [sp, #-16]!\n");
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 2) {
@@ -146,25 +267,19 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    // トークナイズしてパースする
     user_input = argv[1];
-
     token = tokenize(user_input);
+    Node *node = expr();
 
     printf(".text\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov x0, #%d\n", expect_number());
 
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("  add x0, x0, #%d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("  sub x0, x0, #%d\n", expect_number());
-    }
-
+    // 抽象構文木を下りながらコード生成
+    gen(node);
+    
+    printf("  ldr x0, [sp], #16\n");
     printf("  mov x8, #93\n");
     printf("  svc #0\n");
     return 0;
